efbd02ef1218d6f30a1f23f8756edb321892bd7f
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-03-24 11:54:04 +0100

    Add files via upload

---
+++ nasljedivanje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik u1 = new Ucenik();
+            u1.Ime = "Marko";
+            u1.Prezime = "Marulić";
+            u1.Razred = 3;
+
+            Nastavnik n1 = new Nastavnik();
+            n1.Ime = "MrKee ";
+            n1.Prezime = "ItMaster";
+            n1.Predmet = "Programiranje";
+              
+
+
+
+        }
+    }
+}
